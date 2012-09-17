f18408b6d8150216c98fe254aff10e3a04c00b84
André Brás <andrebras@gmail.com>
2012-09-17 11:35:29 +0100

    New (recommended) manage.py file
    "The previous manage.py called functions that are now deprecated"
    
    https://docs.djangoproject.com/en/1.4/releases/1.4/#updated-default-proj
    ect-layout-and-manage-py

--- mysite/manage.py
+++ mysite/manage.py
@@ -4,22 +4,20 @@
 import os
 import os.path
 import sys
-THIS_FILE=os.path.abspath(__file__)
-THIS_DIR=os.path.dirname(THIS_FILE)
-UP_ONE_DIR=os.path.join(THIS_DIR, '..')
+
+THIS_FILE   = os.path.abspath(__file__)
+THIS_DIR    = os.path.dirname(THIS_FILE)
+UP_ONE_DIR  = os.path.join(THIS_DIR, '..')
+
 sys.path.append(UP_ONE_DIR)
 
 # Use the modules in vendor/
 import vendor
 vendor.vendorify()
 
-# Now we can import from third-party libraries.
-from django.core.management import execute_manager, setup_environ
-
-import mysite.settings
-
-# The first thing execute_manager does is call `setup_environ`.
-setup_environ(mysite.settings)
-
 if __name__ == "__main__":
-    execute_manager(mysite.settings)
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
